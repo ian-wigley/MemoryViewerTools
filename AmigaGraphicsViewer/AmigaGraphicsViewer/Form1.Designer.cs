fd96d4e1c941586031688a1ccf43da3a0f3886ba
Ian Wigley <ian.wigley@outlook.com>
2023-01-31 09:06:03 +0000

    Update Form1.Designer.cs
    Update Namespace and class name.

--- AmigaGraphicsViewer/AmigaGraphicsViewer/Form1.Designer.cs
+++ AmigaGraphicsViewer/AmigaGraphicsViewer/Form1.Designer.cs
@@ -1,9 +1,9 @@
 ﻿using System.Windows.Forms;
 using System.ComponentModel.Design;
 
-namespace MemoryViewer
+namespace MemoryAndGraphicsViewer
 {
-    partial class Form1
+    partial class GraphicsViewer
     {
         /// <summary>
         /// Required designer variable.
@@ -68,7 +68,8 @@ namespace MemoryViewer
             this.editToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1045, 28);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(784, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -79,27 +80,27 @@ namespace MemoryViewer
             this.saveAsToolStripMenuItem,
             this.exitToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
             this.fileToolStripMenuItem.Text = "File";
             // 
             // loadToolStripMenuItem
             // 
             this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
-            this.loadToolStripMenuItem.Size = new System.Drawing.Size(143, 26);
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
             this.loadToolStripMenuItem.Text = "Load";
             this.loadToolStripMenuItem.Click += new System.EventHandler(this.LoadMenuClick);
             // 
             // saveAsToolStripMenuItem
             // 
             this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
-            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(143, 26);
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
             this.saveAsToolStripMenuItem.Text = "Save As";
-            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.SaveAsToolStripMenuItem_Click);
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.SaveAsMenu_Click);
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(143, 26);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
             this.exitToolStripMenuItem.Text = "Exit";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitMenuClick);
             // 
@@ -108,13 +109,13 @@ namespace MemoryViewer
             this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.findToolStripMenuItem});
             this.editToolStripMenuItem.Name = "editToolStripMenuItem";
-            this.editToolStripMenuItem.Size = new System.Drawing.Size(49, 24);
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
             this.editToolStripMenuItem.Text = "Edit";
             // 
             // findToolStripMenuItem
             // 
             this.findToolStripMenuItem.Name = "findToolStripMenuItem";
-            this.findToolStripMenuItem.Size = new System.Drawing.Size(120, 26);
+            this.findToolStripMenuItem.Size = new System.Drawing.Size(97, 22);
             this.findToolStripMenuItem.Text = "Find";
             this.findToolStripMenuItem.Click += new System.EventHandler(this.FindToolStripMenuItem_Click);
             // 
@@ -122,21 +123,19 @@ namespace MemoryViewer
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(0, 33);
-            this.tabControl1.Margin = new System.Windows.Forms.Padding(4);
+            this.tabControl1.Location = new System.Drawing.Point(0, 27);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1045, 1006);
+            this.tabControl1.Size = new System.Drawing.Size(784, 817);
             this.tabControl1.TabIndex = 4;
             this.tabControl1.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.TabControl1_DrawItem);
             // 
             // tabPage1
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 25);
-            this.tabPage1.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(4);
-            this.tabPage1.Size = new System.Drawing.Size(1037, 977);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage1.Size = new System.Drawing.Size(776, 791);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Memory Viewer";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -144,11 +143,10 @@ namespace MemoryViewer
             // tabPage2
             // 
             this.tabPage2.Controls.Add(this.groupBox1);
-            this.tabPage2.Location = new System.Drawing.Point(4, 25);
-            this.tabPage2.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(4);
-            this.tabPage2.Size = new System.Drawing.Size(1037, 977);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage2.Size = new System.Drawing.Size(776, 791);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Bitmap Viewer";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -165,11 +163,9 @@ namespace MemoryViewer
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.VScrollBar);
             this.groupBox1.Controls.Add(this.canvas);
-            this.groupBox1.Location = new System.Drawing.Point(11, 7);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Location = new System.Drawing.Point(8, 6);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox1.Size = new System.Drawing.Size(1013, 575);
+            this.groupBox1.Size = new System.Drawing.Size(760, 467);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Memory";
@@ -177,96 +173,93 @@ namespace MemoryViewer
             // bmapHeight
             // 
             this.bmapHeight.AutoSize = true;
-            this.bmapHeight.Location = new System.Drawing.Point(25, 114);
+            this.bmapHeight.Location = new System.Drawing.Point(19, 93);
+            this.bmapHeight.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.bmapHeight.Name = "bmapHeight";
-            this.bmapHeight.Size = new System.Drawing.Size(53, 17);
+            this.bmapHeight.Size = new System.Drawing.Size(41, 13);
             this.bmapHeight.TabIndex = 11;
             this.bmapHeight.Text = "Height:";
             // 
             // bmapWidth
             // 
             this.bmapWidth.AutoSize = true;
-            this.bmapWidth.Location = new System.Drawing.Point(25, 92);
+            this.bmapWidth.Location = new System.Drawing.Point(19, 75);
+            this.bmapWidth.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.bmapWidth.Name = "bmapWidth";
-            this.bmapWidth.Size = new System.Drawing.Size(48, 17);
+            this.bmapWidth.Size = new System.Drawing.Size(38, 13);
             this.bmapWidth.TabIndex = 10;
             this.bmapWidth.Text = "Width:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(25, 69);
+            this.label5.Location = new System.Drawing.Point(19, 56);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(84, 17);
+            this.label5.Size = new System.Drawing.Size(63, 13);
             this.label5.TabIndex = 9;
             this.label5.Text = "Bitmap size:";
             // 
             // CopyDataLabel
             // 
             this.CopyDataLabel.AutoSize = true;
-            this.CopyDataLabel.Location = new System.Drawing.Point(432, 458);
-            this.CopyDataLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.CopyDataLabel.Location = new System.Drawing.Point(324, 372);
             this.CopyDataLabel.Name = "CopyDataLabel";
-            this.CopyDataLabel.Size = new System.Drawing.Size(162, 17);
+            this.CopyDataLabel.Size = new System.Drawing.Size(124, 13);
             this.CopyDataLabel.TabIndex = 8;
             this.CopyDataLabel.Text = "Copy Screen Data to IFF";
             // 
             // Copy
             // 
-            this.Copy.Location = new System.Drawing.Point(459, 478);
-            this.Copy.Margin = new System.Windows.Forms.Padding(4);
+            this.Copy.Location = new System.Drawing.Point(344, 388);
             this.Copy.Name = "Copy";
-            this.Copy.Size = new System.Drawing.Size(100, 28);
+            this.Copy.Size = new System.Drawing.Size(75, 23);
             this.Copy.TabIndex = 7;
             this.Copy.Text = "Copy";
             this.Copy.UseVisualStyleBackColor = true;
-            this.Copy.Click += new System.EventHandler(this.Button1_Click);
+            this.Copy.Click += new System.EventHandler(this.IFFButton_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(228, 46);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(171, 37);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(21, 17);
+            this.label3.Size = new System.Drawing.Size(16, 13);
             this.label3.TabIndex = 6;
             this.label3.Text = "->";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(24, 48);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(18, 39);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(124, 17);
+            this.label2.Size = new System.Drawing.Size(94, 13);
             this.label2.TabIndex = 5;
             this.label2.Text = "Memory Location :";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(157, 48);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(118, 39);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(62, 17);
+            this.label1.Size = new System.Drawing.Size(48, 13);
             this.label1.TabIndex = 4;
             this.label1.Text = "0x00000";
             // 
             // VScrollBar
             // 
             this.VScrollBar.LargeChange = 1;
-            this.VScrollBar.Location = new System.Drawing.Point(701, 111);
+            this.VScrollBar.Location = new System.Drawing.Point(526, 90);
             this.VScrollBar.Name = "VScrollBar";
-            this.VScrollBar.Size = new System.Drawing.Size(22, 214);
+            this.VScrollBar.Size = new System.Drawing.Size(22, 174);
             this.VScrollBar.TabIndex = 3;
             this.VScrollBar.Scroll += new System.Windows.Forms.ScrollEventHandler(this.VScrollBar_Scroll);
             // 
             // canvas
             // 
-            this.canvas.Location = new System.Drawing.Point(253, 52);
-            this.canvas.Margin = new System.Windows.Forms.Padding(4);
+            this.canvas.Location = new System.Drawing.Point(190, 42);
             this.canvas.Name = "canvas";
-            this.canvas.Size = new System.Drawing.Size(427, 315);
+            this.canvas.Size = new System.Drawing.Size(320, 256);
             this.canvas.TabIndex = 1;
             this.canvas.TabStop = false;
             // 
@@ -278,18 +271,17 @@ namespace MemoryViewer
             this.groupBox2.TabIndex = 0;
             this.groupBox2.TabStop = false;
             // 
-            // Form1
+            // GraphicsViewer
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1045, 1035);
+            this.ClientSize = new System.Drawing.Size(784, 841);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MainMenuStrip = this.menuStrip1;
-            this.Margin = new System.Windows.Forms.Padding(4);
-            this.Name = "Form1";
-            this.Text = "Memory Viewer";
+            this.Name = "GraphicsViewer";
+            this.Text = "Memory & Graphical Viewer";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
