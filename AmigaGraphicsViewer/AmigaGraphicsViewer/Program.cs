26b3642988dbc1e8e99ad0894332662081792e79
Ian Wigley <ian.wigley@outlook.com>
2023-01-31 09:07:06 +0000

    Update Program.cs
    Update Namespace and Application name.

--- AmigaGraphicsViewer/AmigaGraphicsViewer/Program.cs
+++ AmigaGraphicsViewer/AmigaGraphicsViewer/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Windows.Forms;
 
-namespace MemoryViewer
+namespace MemoryAndGraphicsViewer
 {
     static class Program
     {
@@ -13,7 +13,7 @@ namespace MemoryViewer
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new GraphicsViewer());
         }
     }
 }
