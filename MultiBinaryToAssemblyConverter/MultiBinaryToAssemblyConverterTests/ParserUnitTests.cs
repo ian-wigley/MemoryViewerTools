7f5ed3da2b4bab23f02b9866f78b7f7ff85b8dfa
Ian Wigley <ian.wigley@outlook.com>
2023-02-09 08:14:50 +0000

    Updates to add Test Framework.
    Updates to add Test Framework.

---
+++ MultiBinaryToAssemblyConverter/MultiBinaryToAssemblyConverterTests/ParserUnitTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using BinToAssembly;
+
+//https://learn.microsoft.com/en-us/visualstudio/test/getting-started-with-unit-testing?view=vs-2022&tabs=dotnet%2Cmstest
+
+namespace MultiBinaryToAssemblyConverterTests
+{
+    [TestClass]
+    public class ParserUnitTests
+    {
+        [TestMethod]
+        public void Test68000Parser()
+        {
+            Parser68000 parser = new Parser68000();
+            Assert.IsNotNull(parser);
+        }
+    }
+}
