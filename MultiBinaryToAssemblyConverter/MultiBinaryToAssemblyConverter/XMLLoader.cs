99b0f1b62506606a23d494e097d46bacc3acca8b
Ian Wigley <ian.wigley@outlook.com>
2024-11-13 18:25:36 +0000

    Update XMLLoader.cs

--- MultiBinaryToAssemblyConverter/MultiBinaryToAssemblyConverter/XMLLoader.cs
+++ MultiBinaryToAssemblyConverter/MultiBinaryToAssemblyConverter/XMLLoader.cs
@@ -27,7 +27,7 @@ namespace BinToAssembly
                             // Split the line using the delimiter
                             string[] split = reader.Value.Split('¬');
                             var method = split[1].Replace(".", "_");
-                            m_OpCodes.Add(new OpCode(split[0], split[1], int.Parse(split[2]), split[3], split[4], split[5], method));
+                            m_OpCodes.Add(new OpCode(split[0], split[1], int.Parse(split[2]), split[3], split[4], split[5], split[6], method));
                         }
 
                         if (reader.Value.Equals(processor))
