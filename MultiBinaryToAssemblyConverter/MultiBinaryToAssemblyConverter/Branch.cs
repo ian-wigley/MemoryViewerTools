bb5c8a9edc6acd4b589eb3de913b633e99c8a67c
Ian Wigley <ian.wigley@outlook.com>
2024-11-28 14:07:39 +0000

    Update Branch.cs

--- MultiBinaryToAssemblyConverter/MultiBinaryToAssemblyConverter/Branch.cs
+++ MultiBinaryToAssemblyConverter/MultiBinaryToAssemblyConverter/Branch.cs
@@ -1,6 +1,8 @@
-﻿namespace BinToAssembly
+﻿using System;
+
+namespace BinToAssembly
 {
-    public class Branch : OpCode
+    public class Branch : BaseOpCode
     {
         public Branch(
             string code,
@@ -11,18 +13,49 @@
             string midfix,
             string suffix,
             string methodName
-            ) : base(code, name, numberOfBytes, prefix, firstfix, midfix, suffix, methodName)
+            )
         {
+            m_code = code;
+            m_name = name;
+            m_numberOfBytes = numberOfBytes;
+            m_prefix = prefix;
+            m_firstfix = firstfix;
+            m_midfix = midfix;
+            m_suffix = suffix;
+            m_methodName = methodName;
+            Type type = typeof(OpCode);
+            m_methodInfo = type.GetMethod(methodName);
         }
-
 
         public new string Detail(ref int filePosition, byte[] binaryFileData)
         {
             string elementOne = "";
             string elementTwo = "";
             string elementThree = "";
-            string elementFour = "";
-            return "";
+
+            if (NumberOfBytes == 1)
+            {
+                sbyte amount = unchecked((sbyte)binaryFileData[filePosition + 1]);
+                int result = filePosition + 2 + amount;
+                elementOne = result.ToString("X4");
+                filePosition += 2;
+            }
+
+            if (NumberOfBytes == 2)
+            {
+                filePosition += 2;
+                elementOne = ((short)binaryFileData[filePosition++]).ToString("X2") +
+                    ((short)binaryFileData[filePosition++]).ToString("X2");
+                var amount = Convert.ToInt16(elementOne, 16);
+                int result = filePosition - NumberOfBytes + amount;
+                elementOne = result.ToString("X4");
+            }
+
+            string binOne = !elementOne.Equals("") ? elementOne : pad;
+            string binTwo = !elementTwo.Equals("") ? elementTwo : pad;
+
+            string retunLine = " " + binOne + " " + binTwo + "          " + Name + " " + Prefix + elementOne + Firstfix + elementTwo + Midfix + elementThree + Suffix;
+            return retunLine;
         }
     }
 }
