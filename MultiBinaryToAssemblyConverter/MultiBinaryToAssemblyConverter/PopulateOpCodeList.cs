a759c6f8333cbbd1f8d59761333ad9e1fd77ce3f
Ian Wigley <ian.wigley@outlook.com>
2024-11-28 14:08:40 +0000

    Update PopulateOpCodeList.cs

--- MultiBinaryToAssemblyConverter/MultiBinaryToAssemblyConverter/PopulateOpCodeList.cs
+++ MultiBinaryToAssemblyConverter/MultiBinaryToAssemblyConverter/PopulateOpCodeList.cs
@@ -5,13 +5,13 @@ namespace BinToAssembly
 {
     public class PopulateOpCodeList
     {
-        private readonly List<OpCode> m_OpCodes = new List<OpCode>();
-        public List<OpCode> GetOpCodes { get { return m_OpCodes; } }
+        private readonly List<BaseOpCode> m_OpCodes = new List<BaseOpCode>();
+        public List<BaseOpCode> GetOpCodes { get { return m_OpCodes; } }
         private readonly XMLLoader xmlLoader = new XMLLoader();
         private string processor = "";
         public string GetProcessor { get { return processor; } }
 
-        public OpCode GetOpCode(string value)
+        public dynamic GetOpCode(string value)
         {
             return m_OpCodes.FirstOrDefault(opCode => opCode.Code.Equals(value));
         }
