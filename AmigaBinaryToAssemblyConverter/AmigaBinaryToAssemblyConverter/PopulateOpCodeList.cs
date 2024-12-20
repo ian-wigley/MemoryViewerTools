c5fb3fa4013db5a444058e2d32aefb56917b28b9
Ian Wigley <ian.wigley@outlook.com>
2024-12-20 11:16:48 +0000

    Create PopulateOpCodeList.cs

---
+++ AmigaBinaryToAssemblyConverter/AmigaBinaryToAssemblyConverter/PopulateOpCodeList.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BinToAssembly
+{
+    public class PopulateOpCodeList
+    {
+        private readonly List<BaseOpCode> m_OpCodes = new List<BaseOpCode>();
+        public List<BaseOpCode> GetOpCodes { get { return m_OpCodes; } }
+        private readonly XMLLoader xmlLoader = new XMLLoader();
+        private string processor = "";
+        public string GetProcessor { get { return processor; } }
+
+        public void Init(string processor)
+        {
+            this.processor = processor;
+            m_OpCodes.Clear();
+            xmlLoader.SetValid = false;
+            xmlLoader.LoadSettings(processor);
+            xmlLoader.LoadOpCodes(m_OpCodes, processor);
+        }
+
+        //public dynamic GetOpCode(string value)
+        //{
+        //    return m_OpCodes.FirstOrDefault(opCode => opCode.Code.Equals(value));
+        //}
+
+        public dynamic GetOpCode(string t, string tt)
+        {
+            return m_OpCodes.FirstOrDefault(opCode => opCode.m_codeOne.Equals(t) && opCode.m_codeTwo.Equals(tt));
+
+        }
+
+    }
+}
