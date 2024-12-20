c88c99dda666ee6dd06773a35148e0c7b814b2aa
Ian Wigley <ian.wigley@outlook.com>
2024-12-20 11:16:45 +0000

    Create OpCode.cs

---
+++ AmigaBinaryToAssemblyConverter/AmigaBinaryToAssemblyConverter/OpCode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BinToAssembly
+{
+    public class OpCode : BaseOpCode
+    {
+        public OpCode(
+            string codeOne,
+            string codeTwo,
+            string name,
+            int numberOfBytes,
+            string prefix,
+            string firstfix,
+            string midfix,
+            string suffix,
+            string dataSize
+            )
+        {
+            m_codeOne = codeOne;
+            m_codeTwo = codeTwo;
+            m_code = codeOne + codeTwo;
+            m_name = name;
+            m_numberOfBytes = numberOfBytes;
+            m_prefix = prefix;
+            m_firstfix = firstfix;
+            m_midfix = midfix;
+            m_suffix = suffix;
+            m_dataSize = dataSize;
+        }
+    }
+}
