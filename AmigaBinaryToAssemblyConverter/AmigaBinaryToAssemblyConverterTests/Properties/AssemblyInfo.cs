8eefd60fc2970f4224742b90b9006d8ccf6c86fd
Ian Wigley <ian.wigley@outlook.com>
2024-12-22 16:41:41 +0000

    Create AssemblyInfo.cs

---
+++ AmigaBinaryToAssemblyConverter/AmigaBinaryToAssemblyConverterTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MultiBinaryToAssemblyConverterTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MultiBinaryToAssemblyConverterTests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e5c66e27-4eba-4afb-95b3-45523e3258cc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
