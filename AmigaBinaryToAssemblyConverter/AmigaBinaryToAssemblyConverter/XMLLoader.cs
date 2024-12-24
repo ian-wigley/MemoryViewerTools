c3b2a1faea830798fd664682465091ee0219612d
Ian Wigley <ian.wigley@outlook.com>
2024-12-24 12:24:03 +0000

    Update XMLLoader.cs

--- AmigaBinaryToAssemblyConverter/AmigaBinaryToAssemblyConverter/XMLLoader.cs
+++ AmigaBinaryToAssemblyConverter/AmigaBinaryToAssemblyConverter/XMLLoader.cs
@@ -62,10 +62,19 @@ namespace BinToAssembly
                             string[] split = reader.Value.Split('¬');
                             string name = split[2];
                             GetDataType(name, out string dataSize);
-                            if (name.Contains("BNE") || name.Contains("BEQ") || name.Contains("JSR") || name.Contains("BSR"))
+                            //if (name.Contains("BNE") || name.Contains("BEQ") || name.Contains("JSR") || name.Contains("BSR"))
+                            if (name.Contains("BNE") || name.Contains("BEQ"))
                             {
                                 m_OpCodes.Add(new Branch(split[0], split[1], name, int.Parse(split[3]), split[4], split[5], split[6], split[7], dataSize));
                             }
+                            else if (name.Contains("JSR") || name.Contains("BSR"))
+                            {
+                                m_OpCodes.Add(new Jump(split[0], split[1], name, int.Parse(split[3]), split[4], split[5], split[6], split[7], dataSize));
+                            }
+                            //if (name.Contains("MOVE"))
+                            //{
+                            //    m_OpCodes.Add(new Move(split[0], split[1], name, int.Parse(split[3]), split[4], split[5], split[6], split[7], dataSize));
+                            //}
                             else
                             {
                                 m_OpCodes.Add(new OpCode(split[0], split[1], name, int.Parse(split[3]), split[4], split[5], split[6], split[7], dataSize));
