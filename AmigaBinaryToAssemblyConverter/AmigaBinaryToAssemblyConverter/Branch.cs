ee2c3fb23a2580c47670657ba2873e5a328a26b8
Ian Wigley <ian.wigley@outlook.com>
2024-12-25 08:53:07 +0000

    Update Branch.cs

--- AmigaBinaryToAssemblyConverter/AmigaBinaryToAssemblyConverter/Branch.cs
+++ AmigaBinaryToAssemblyConverter/AmigaBinaryToAssemblyConverter/Branch.cs
@@ -45,7 +45,7 @@ namespace BinToAssembly
                 filePosition += 2;
             }
 
-            if (NumberOfBytes == 2 && !m_name.Equals("JSR"))
+            if (NumberOfBytes == 2)
             {
                 filePosition += 2;
                 elementOne = GetTwoShorts(ref filePosition, binaryFileData);
@@ -54,27 +54,6 @@ namespace BinToAssembly
                 binOne = elementOne;
                 elementOne = result.ToString("x4");
             }
-
-            //if (NumberOfBytes == 2 && m_name.Equals("JSR"))
-            //{
-            //    filePosition += 2;
-            //    elementOne = GetTwoShorts(ref filePosition, binaryFileData);
-            //    var amount = Convert.ToInt16(elementOne, 16);
-            //    binOne = elementOne;
-            //    elementOne = amount.ToString();
-            //}
-
-            //if (NumberOfBytes == 4 && m_name.Equals("JSR"))
-            //{
-            //    filePosition += 2;
-            //    elementOne = GetTwoShorts(ref filePosition, binaryFileData);
-            //    var amount = Convert.ToInt16(elementOne, 16);
-            //    binOne = elementOne;
-            //    elementOne = amount.ToString();
-            //    filePosition += 2;
-            //    elementTwo = GetTwoShorts(ref filePosition, binaryFileData);
-            //    filePosition += 2;
-            //}
 
             string retunLine = " " + binOne + " " + binTwo + "          " + Name + " " + Prefix + elementOne + Firstfix + elementTwo + Midfix + elementThree + Suffix;
             return retunLine;
