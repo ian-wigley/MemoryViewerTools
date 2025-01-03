f172af9dff5747357a4dad128f2815ef9c1a41b6
Ian Wigley <ian.wigley@outlook.com>
2025-01-03 18:40:27 +0000

    Update MemorySelector.cs

--- AmigaBinaryToAssemblyConverter/AmigaBinaryToAssemblyConverter/MemorySelector.cs
+++ AmigaBinaryToAssemblyConverter/AmigaBinaryToAssemblyConverter/MemorySelector.cs
@@ -9,11 +9,24 @@ namespace BinToAssembly
         private char[] m_startAdress;
         private char[] m_endAdress;
 
-        public string GetSelectedMemStartLocation { get { return comboBox1.Text + comboBox2.Text + comboBox3.Text + comboBox4.Text; } }
-        public string GetSelectedMemEndLocation { get { return comboBox5.Text + comboBox6.Text + comboBox7.Text + comboBox8.Text; } }
+        public string GetSelectedMemStartLocation
+        {
+            get
+            {
+                return
+                    startAddress1.Text + startAddress2.Text + startAddress3.Text + startAddress4.Text + startAddress5.Text + startAddress6.Text + startAddress7.Text + startAddress8.Text;
+            }
+        }
+        public string GetSelectedMemEndLocation { 
+            get 
+            { 
+                return 
+                    endAddress1.Text + endAddress2.Text + endAddress3.Text + endAddress4.Text + endAddress5.Text + endAddress6.Text + endAddress7.Text + endAddress8.Text;
+            } 
+        }
 
         public MemorySelector(
-            char[] startAdress, 
+            char[] startAdress,
             char[] endAdress)
         {
             InitializeComponent();
@@ -30,26 +43,45 @@ namespace BinToAssembly
                 memValues.Add(new { Text = i.ToString("X1"), Value = i });
             }
 
-            InitialiseComboBoxes(comboBox1);
-            InitialiseComboBoxes(comboBox2);
-            InitialiseComboBoxes(comboBox3);
-            InitialiseComboBoxes(comboBox4);
-            InitialiseComboBoxes(comboBox5);
-            InitialiseComboBoxes(comboBox6);
-            InitialiseComboBoxes(comboBox7);
-            InitialiseComboBoxes(comboBox8);
+            InitialiseComboBoxes(startAddress1);
+            InitialiseComboBoxes(startAddress2);
+            InitialiseComboBoxes(startAddress3);
+            InitialiseComboBoxes(startAddress4);
 
-            comboBox1.Text = startAdress[0].ToString();
-            comboBox2.Text = startAdress[1].ToString();
-            comboBox3.Text = startAdress[2].ToString();
-            comboBox4.Text = startAdress[3].ToString();
+            InitialiseComboBoxes(startAddress5);
+            InitialiseComboBoxes(startAddress6);
+            InitialiseComboBoxes(startAddress7);
+            InitialiseComboBoxes(startAddress8);
 
-            comboBox5.Text = endAdress[0].ToString();
-            comboBox6.Text = endAdress[1].ToString();
-            comboBox7.Text = endAdress[2].ToString();
-            comboBox8.Text = endAdress[3].ToString();
+            InitialiseComboBoxes(endAddress1);
+            InitialiseComboBoxes(endAddress2);
+            InitialiseComboBoxes(endAddress3);
+            InitialiseComboBoxes(endAddress4);
 
-            checkBox1.Enabled = false;
+            InitialiseComboBoxes(endAddress5);
+            InitialiseComboBoxes(endAddress6);
+            InitialiseComboBoxes(endAddress7);
+            InitialiseComboBoxes(endAddress8);
+
+            startAddress1.Text = startAdress[0].ToString();
+            startAddress2.Text = startAdress[1].ToString();
+            startAddress3.Text = startAdress[2].ToString();
+            startAddress4.Text = startAdress[3].ToString();
+
+            startAddress5.Text = startAdress[4].ToString();
+            startAddress6.Text = startAdress[5].ToString();
+            startAddress7.Text = startAdress[6].ToString();
+            startAddress8.Text = startAdress[7].ToString();
+
+            endAddress1.Text = endAdress[0].ToString();
+            endAddress2.Text = endAdress[1].ToString();
+            endAddress3.Text = endAdress[2].ToString();
+            endAddress4.Text = endAdress[3].ToString();
+
+            endAddress5.Text = endAdress[4].ToString();
+            endAddress6.Text = endAdress[5].ToString();
+            endAddress7.Text = endAdress[6].ToString();
+            endAddress8.Text = endAdress[7].ToString();
         }
 
         private void InitialiseComboBoxes(
@@ -62,10 +94,10 @@ namespace BinToAssembly
         }
 
         private void Button1_Click(
-            object sender, 
+            object sender,
             System.EventArgs e)
         {
-            this.DialogResult = DialogResult.OK;
+            DialogResult = DialogResult.OK;
             Close();
         }
     }
