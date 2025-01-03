c8ae733738b5be71da33a14d9255950669b224bd
Ian Wigley <ian.wigley@outlook.com>
2025-01-03 18:40:31 +0000

    Update MemorySelector.Designer.cs

--- AmigaBinaryToAssemblyConverter/AmigaBinaryToAssemblyConverter/MemorySelector.Designer.cs
+++ AmigaBinaryToAssemblyConverter/AmigaBinaryToAssemblyConverter/MemorySelector.Designer.cs
@@ -30,119 +30,134 @@
         {
             this.components = new System.ComponentModel.Container();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.comboBox4 = new System.Windows.Forms.ComboBox();
-            this.comboBox3 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.startAddress4 = new System.Windows.Forms.ComboBox();
+            this.startAddress3 = new System.Windows.Forms.ComboBox();
+            this.startAddress2 = new System.Windows.Forms.ComboBox();
+            this.startAddress1 = new System.Windows.Forms.ComboBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.comboBox8 = new System.Windows.Forms.ComboBox();
-            this.comboBox7 = new System.Windows.Forms.ComboBox();
-            this.comboBox6 = new System.Windows.Forms.ComboBox();
-            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.endAddress4 = new System.Windows.Forms.ComboBox();
+            this.endAddress3 = new System.Windows.Forms.ComboBox();
+            this.endAddress2 = new System.Windows.Forms.ComboBox();
+            this.endAddress1 = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.startAddress5 = new System.Windows.Forms.ComboBox();
+            this.startAddress6 = new System.Windows.Forms.ComboBox();
+            this.startAddress7 = new System.Windows.Forms.ComboBox();
+            this.startAddress8 = new System.Windows.Forms.ComboBox();
+            this.endAddress8 = new System.Windows.Forms.ComboBox();
+            this.endAddress7 = new System.Windows.Forms.ComboBox();
+            this.endAddress6 = new System.Windows.Forms.ComboBox();
+            this.endAddress5 = new System.Windows.Forms.ComboBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.comboBox4);
-            this.groupBox1.Controls.Add(this.comboBox3);
-            this.groupBox1.Controls.Add(this.comboBox2);
-            this.groupBox1.Controls.Add(this.comboBox1);
-            this.groupBox1.Location = new System.Drawing.Point(32, 12);
+            this.groupBox1.Controls.Add(this.startAddress8);
+            this.groupBox1.Controls.Add(this.startAddress7);
+            this.groupBox1.Controls.Add(this.startAddress6);
+            this.groupBox1.Controls.Add(this.startAddress5);
+            this.groupBox1.Controls.Add(this.startAddress4);
+            this.groupBox1.Controls.Add(this.startAddress3);
+            this.groupBox1.Controls.Add(this.startAddress2);
+            this.groupBox1.Controls.Add(this.startAddress1);
+            this.groupBox1.Location = new System.Drawing.Point(5, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(208, 62);
+            this.groupBox1.Size = new System.Drawing.Size(276, 62);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Select Memory Start Address";
             // 
-            // comboBox4
+            // startAddress4
             // 
-            this.comboBox4.DropDownWidth = 30;
-            this.comboBox4.FormattingEnabled = true;
-            this.comboBox4.Location = new System.Drawing.Point(154, 30);
-            this.comboBox4.Name = "comboBox4";
-            this.comboBox4.Size = new System.Drawing.Size(40, 21);
-            this.comboBox4.TabIndex = 4;
+            this.startAddress4.DropDownWidth = 30;
+            this.startAddress4.FormattingEnabled = true;
+            this.startAddress4.Location = new System.Drawing.Point(105, 30);
+            this.startAddress4.Name = "startAddress4";
+            this.startAddress4.Size = new System.Drawing.Size(30, 21);
+            this.startAddress4.TabIndex = 4;
             // 
-            // comboBox3
+            // startAddress3
             // 
-            this.comboBox3.DropDownWidth = 30;
-            this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(108, 30);
-            this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(40, 21);
-            this.comboBox3.TabIndex = 3;
+            this.startAddress3.DropDownWidth = 30;
+            this.startAddress3.FormattingEnabled = true;
+            this.startAddress3.Location = new System.Drawing.Point(72, 30);
+            this.startAddress3.Name = "startAddress3";
+            this.startAddress3.Size = new System.Drawing.Size(30, 21);
+            this.startAddress3.TabIndex = 3;
             // 
-            // comboBox2
+            // startAddress2
             // 
-            this.comboBox2.DropDownWidth = 30;
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(62, 30);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(40, 21);
-            this.comboBox2.TabIndex = 2;
+            this.startAddress2.DropDownWidth = 30;
+            this.startAddress2.FormattingEnabled = true;
+            this.startAddress2.Location = new System.Drawing.Point(39, 30);
+            this.startAddress2.Name = "startAddress2";
+            this.startAddress2.Size = new System.Drawing.Size(30, 21);
+            this.startAddress2.TabIndex = 2;
             // 
-            // comboBox1
+            // startAddress1
             // 
-            this.comboBox1.DropDownWidth = 30;
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(16, 30);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(40, 21);
-            this.comboBox1.TabIndex = 1;
+            this.startAddress1.DropDownWidth = 30;
+            this.startAddress1.FormattingEnabled = true;
+            this.startAddress1.Location = new System.Drawing.Point(6, 30);
+            this.startAddress1.Name = "startAddress1";
+            this.startAddress1.Size = new System.Drawing.Size(30, 21);
+            this.startAddress1.TabIndex = 1;
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.comboBox8);
-            this.groupBox2.Controls.Add(this.comboBox7);
-            this.groupBox2.Controls.Add(this.comboBox6);
-            this.groupBox2.Controls.Add(this.comboBox5);
-            this.groupBox2.Location = new System.Drawing.Point(32, 91);
+            this.groupBox2.Controls.Add(this.endAddress8);
+            this.groupBox2.Controls.Add(this.endAddress7);
+            this.groupBox2.Controls.Add(this.endAddress6);
+            this.groupBox2.Controls.Add(this.endAddress5);
+            this.groupBox2.Controls.Add(this.endAddress4);
+            this.groupBox2.Controls.Add(this.endAddress3);
+            this.groupBox2.Controls.Add(this.endAddress2);
+            this.groupBox2.Controls.Add(this.endAddress1);
+            this.groupBox2.Location = new System.Drawing.Point(5, 91);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(208, 72);
+            this.groupBox2.Size = new System.Drawing.Size(276, 64);
             this.groupBox2.TabIndex = 5;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Select Memory End Address";
             // 
-            // comboBox8
+            // endAddress4
             // 
-            this.comboBox8.FormattingEnabled = true;
-            this.comboBox8.Location = new System.Drawing.Point(154, 29);
-            this.comboBox8.Name = "comboBox8";
-            this.comboBox8.Size = new System.Drawing.Size(40, 21);
-            this.comboBox8.TabIndex = 8;
+            this.endAddress4.FormattingEnabled = true;
+            this.endAddress4.Location = new System.Drawing.Point(104, 29);
+            this.endAddress4.Name = "endAddress4";
+            this.endAddress4.Size = new System.Drawing.Size(30, 21);
+            this.endAddress4.TabIndex = 8;
             // 
-            // comboBox7
+            // endAddress3
             // 
-            this.comboBox7.FormattingEnabled = true;
-            this.comboBox7.Location = new System.Drawing.Point(108, 29);
-            this.comboBox7.Name = "comboBox7";
-            this.comboBox7.Size = new System.Drawing.Size(40, 21);
-            this.comboBox7.TabIndex = 7;
+            this.endAddress3.FormattingEnabled = true;
+            this.endAddress3.Location = new System.Drawing.Point(71, 29);
+            this.endAddress3.Name = "endAddress3";
+            this.endAddress3.Size = new System.Drawing.Size(30, 21);
+            this.endAddress3.TabIndex = 7;
             // 
-            // comboBox6
+            // endAddress2
             // 
-            this.comboBox6.FormattingEnabled = true;
-            this.comboBox6.Location = new System.Drawing.Point(62, 29);
-            this.comboBox6.Name = "comboBox6";
-            this.comboBox6.Size = new System.Drawing.Size(40, 21);
-            this.comboBox6.TabIndex = 6;
+            this.endAddress2.FormattingEnabled = true;
+            this.endAddress2.Location = new System.Drawing.Point(38, 29);
+            this.endAddress2.Name = "endAddress2";
+            this.endAddress2.Size = new System.Drawing.Size(30, 21);
+            this.endAddress2.TabIndex = 6;
             // 
-            // comboBox5
+            // endAddress1
             // 
-            this.comboBox5.FormattingEnabled = true;
-            this.comboBox5.Location = new System.Drawing.Point(16, 29);
-            this.comboBox5.Name = "comboBox5";
-            this.comboBox5.Size = new System.Drawing.Size(40, 21);
-            this.comboBox5.TabIndex = 5;
+            this.endAddress1.FormattingEnabled = true;
+            this.endAddress1.Location = new System.Drawing.Point(5, 29);
+            this.endAddress1.Name = "endAddress1";
+            this.endAddress1.Size = new System.Drawing.Size(30, 21);
+            this.endAddress1.TabIndex = 5;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(94, 204);
+            this.button1.Location = new System.Drawing.Point(102, 166);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 6;
@@ -150,22 +165,79 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
-            // checkBox1
+            // startAddress5
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(48, 170);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(173, 17);
-            this.checkBox1.TabIndex = 7;
-            this.checkBox1.Text = "Convert Illegal opcodes to data";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.startAddress5.DropDownWidth = 30;
+            this.startAddress5.FormattingEnabled = true;
+            this.startAddress5.Location = new System.Drawing.Point(138, 30);
+            this.startAddress5.Name = "startAddress5";
+            this.startAddress5.Size = new System.Drawing.Size(30, 21);
+            this.startAddress5.TabIndex = 5;
+            // 
+            // startAddress6
+            // 
+            this.startAddress6.DropDownWidth = 30;
+            this.startAddress6.FormattingEnabled = true;
+            this.startAddress6.Location = new System.Drawing.Point(171, 30);
+            this.startAddress6.Name = "startAddress6";
+            this.startAddress6.Size = new System.Drawing.Size(30, 21);
+            this.startAddress6.TabIndex = 6;
+            // 
+            // startAddress7
+            // 
+            this.startAddress7.DropDownWidth = 30;
+            this.startAddress7.FormattingEnabled = true;
+            this.startAddress7.Location = new System.Drawing.Point(204, 30);
+            this.startAddress7.Name = "startAddress7";
+            this.startAddress7.Size = new System.Drawing.Size(30, 21);
+            this.startAddress7.TabIndex = 7;
+            // 
+            // startAddress8
+            // 
+            this.startAddress8.DropDownWidth = 30;
+            this.startAddress8.FormattingEnabled = true;
+            this.startAddress8.Location = new System.Drawing.Point(237, 30);
+            this.startAddress8.Name = "startAddress8";
+            this.startAddress8.Size = new System.Drawing.Size(30, 21);
+            this.startAddress8.TabIndex = 8;
+            // 
+            // endAddress8
+            // 
+            this.endAddress8.FormattingEnabled = true;
+            this.endAddress8.Location = new System.Drawing.Point(235, 29);
+            this.endAddress8.Name = "endAddress8";
+            this.endAddress8.Size = new System.Drawing.Size(30, 21);
+            this.endAddress8.TabIndex = 12;
+            // 
+            // endAddress7
+            // 
+            this.endAddress7.FormattingEnabled = true;
+            this.endAddress7.Location = new System.Drawing.Point(203, 29);
+            this.endAddress7.Name = "endAddress7";
+            this.endAddress7.Size = new System.Drawing.Size(30, 21);
+            this.endAddress7.TabIndex = 11;
+            // 
+            // endAddress6
+            // 
+            this.endAddress6.FormattingEnabled = true;
+            this.endAddress6.Location = new System.Drawing.Point(170, 29);
+            this.endAddress6.Name = "endAddress6";
+            this.endAddress6.Size = new System.Drawing.Size(30, 21);
+            this.endAddress6.TabIndex = 10;
+            // 
+            // endAddress5
+            // 
+            this.endAddress5.FormattingEnabled = true;
+            this.endAddress5.Location = new System.Drawing.Point(137, 29);
+            this.endAddress5.Name = "endAddress5";
+            this.endAddress5.Size = new System.Drawing.Size(30, 21);
+            this.endAddress5.TabIndex = 9;
             // 
             // MemorySelector
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(275, 245);
-            this.Controls.Add(this.checkBox1);
+            this.ClientSize = new System.Drawing.Size(290, 212);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
@@ -174,24 +246,30 @@
             this.groupBox1.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.ComboBox comboBox3;
-        private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox startAddress3;
+        private System.Windows.Forms.ComboBox startAddress2;
+        private System.Windows.Forms.ComboBox startAddress1;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.ComboBox comboBox8;
-        private System.Windows.Forms.ComboBox comboBox7;
-        private System.Windows.Forms.ComboBox comboBox6;
-        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.ComboBox endAddress4;
+        private System.Windows.Forms.ComboBox endAddress3;
+        private System.Windows.Forms.ComboBox endAddress2;
+        private System.Windows.Forms.ComboBox endAddress1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ComboBox comboBox4;
-        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ComboBox startAddress4;
         private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ComboBox startAddress8;
+        private System.Windows.Forms.ComboBox startAddress7;
+        private System.Windows.Forms.ComboBox startAddress6;
+        private System.Windows.Forms.ComboBox startAddress5;
+        private System.Windows.Forms.ComboBox endAddress8;
+        private System.Windows.Forms.ComboBox endAddress7;
+        private System.Windows.Forms.ComboBox endAddress6;
+        private System.Windows.Forms.ComboBox endAddress5;
     }
 }
