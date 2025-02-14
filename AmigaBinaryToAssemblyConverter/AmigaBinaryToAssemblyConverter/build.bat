f5846c3730528b4b7b05c171800dbeebda27fe8e
Ian Wigley <ian.wigley@outlook.com>
2025-02-14 17:42:22 +0000

    Move the Amiga Binary To Assembly Converter project into it's own repo

--- AmigaBinaryToAssemblyConverter/AmigaBinaryToAssemblyConverter/build.bat
+++
@@ -1,4 +0,0 @@
-﻿C:\Dev\ToolChain\vasm -m68020 -kick1hunks -Fhunkexe -o c:\temp\test.exe "C:\temp\Mega\Start.asm"
-
-rem cmd /c copy c:\temp\test.exe c:\Dev\Machines\A1200Test\DH1\test.exe
-rem cmd /c copy c:\temp\test.exe c:\Dev\Machines\A1200Debug\DH1\test.exe
